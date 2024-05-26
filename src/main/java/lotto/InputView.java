d000179681ae736cf65dcb537e926cccc270d1ac
imellie <ssul4666@naver.com>
2024-05-26 21:52:39 +0900

    1-1.edit and 1-2 write

--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static camp.nextstep.edu.missionutils.Console.readLine;
@@ -11,23 +13,22 @@ public class InputView {
     }
 
     public static int money() {
-        try {
-            amountOfMoney = Integer.parseInt(readLine());
-            if (amountOfMoney % 1000 != 0) {
-                throw new IllegalArgumentException();
-            }
-        } catch (NumberFormatException e) {
+        while (true) {
             try {
-                throw new IllegalArgumentException();
-            } catch (IllegalArgumentException e2) {
-                System.out.println("[ERROR] 잘못된 값을 입력했습니다. 다시 입력해주세요.");
-                money();
+                amountOfMoney = Integer.parseInt(readLine());
+                if (validation(amountOfMoney)) {
+                    break;
+                }
+            } catch (NumberFormatException e) {
+                try {
+                    throw new IllegalArgumentException();
+                } catch (IllegalArgumentException e2) {
+                    System.out.println("[ERROR] 잘못된 값을 입력했습니다. 다시 입력해주세요.");
+                }
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println("[ERROR] 잘못된 값을 입력했습니다. 다시 입력해주세요.");
-            money();
         }
         return amountOfMoney;
+    }
         // * 15라인 안 넘어가도록 구현을 못하겠음
 
         // 구입 금액은 1,000원 단위로 입력 받으며 1,000원으로 나누어 떨어지지 않는 경우 예외 처리한다.
@@ -36,5 +37,31 @@ public class InputView {
 
         // 예외1. 문자->정수 변환이 안 될 경우
         // 예외2. 1000원 단위가 아닌 경우
+
+
+    public static boolean validation(int amountOfMoney){
+        try{
+        if (amountOfMoney % 1000 != 0) {
+            throw new IllegalArgumentException();
+        }
+        }catch (IllegalArgumentException e){
+            System.out.println("[ERROR] 잘못된 값을 입력했습니다. 다시 입력해주세요.");
+            return false;
+        }
+        return true;
+    }
+
+    public static List<Integer> goalNumber(){
+        // string 쉼표로 구분해서 배열에 넣고 싶음
+        String []stringArray = readLine().split(",");
+        int[] intArray = Arrays.stream(stringArray)
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        List<Integer> arrayList = new ArrayList<>();
+        for(int i = 0; i<stringArray.length; i++){
+            arrayList.add(Integer.parseInt(stringArray[i]));
+        }
+        // 변환하기 불편하다
+        return arrayList;
     }
 }
