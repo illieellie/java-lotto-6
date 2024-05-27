f676135357c00fcca86394685480546d8575d7af
imellie <ssul4666@naver.com>
2024-05-27 15:02:49 +0900

    1-3.write

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -6,8 +6,9 @@ import java.util.List;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        int amountOfMoney = InputView.money();
-        List<Integer> goalNumber = InputView.goalNumber();
+        int amountOfMoney = InputView.money(); // 구매 금액
+        List<Integer> goalNumber = InputView.goalNumber(); // 당첨 번호
+        int bonusNumber = InputView.bonusNumber(); // 보너스
         Lotto lotto = new Lotto(goalNumber);
 
 
--- src/main/java/lotto/InputView.java
+++ src/main/java/lotto/InputView.java
@@ -9,6 +9,7 @@ import static camp.nextstep.edu.missionutils.Console.readLine;
 public class InputView {
     // 유틸성 클래스
     static int amountOfMoney = 0;
+
     private InputView() {
     }
 
@@ -29,22 +30,22 @@ public class InputView {
         }
         return amountOfMoney;
     }
-        // * 15라인 안 넘어가도록 구현을 못하겠음
+    // * 15라인 안 넘어가도록 구현을 못하겠음
 
-        // 구입 금액은 1,000원 단위로 입력 받으며 1,000원으로 나누어 떨어지지 않는 경우 예외 처리한다.
-        // 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException를 발생시키고,
-        // "[ERROR]"로 시작하는 에러 메시지를 출력 후 그 부분부터 입력을 다시 받는다.
+    // 구입 금액은 1,000원 단위로 입력 받으며 1,000원으로 나누어 떨어지지 않는 경우 예외 처리한다.
+    // 사용자가 잘못된 값을 입력할 경우 IllegalArgumentException를 발생시키고,
+    // "[ERROR]"로 시작하는 에러 메시지를 출력 후 그 부분부터 입력을 다시 받는다.
 
-        // 예외1. 문자->정수 변환이 안 될 경우
-        // 예외2. 1000원 단위가 아닌 경우
+    // 예외1. 문자->정수 변환이 안 될 경우
+    // 예외2. 1000원 단위가 아닌 경우
 
 
-    public static boolean validationMoney(int amountOfMoney){
-        try{
-        if (amountOfMoney % 1000 != 0) {
-            throw new IllegalArgumentException();
-        }
-        }catch (IllegalArgumentException e){
+    public static boolean validationMoney(int amountOfMoney) {
+        try {
+            if (amountOfMoney % 1000 != 0) {
+                throw new IllegalArgumentException();
+            }
+        } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 잘못된 값을 입력했습니다. 다시 입력해주세요.");
             return false;
         }
@@ -75,17 +76,34 @@ public class InputView {
         return arrayList;
     }
 
-    public static boolean validationGoalNumber(String[] stringArray){
+    public static boolean validationGoalNumber(String[] stringArray) {
         // 예외1. 숫자 여섯개가 맞는지
         try {
             if (stringArray.length != 6) {
                 throw new IllegalArgumentException();
             }
-        }catch(IllegalArgumentException e){
+        } catch (IllegalArgumentException e) {
             System.out.println("[ERROR] 잘못된 값을 입력했습니다. 다시 입력해주세요.");
             return false;
         }
         return true;
     }
 
+    public static int bonusNumber() {
+        int bonusNum = 0;
+        while (true) {
+            String s = readLine();
+            try {
+                bonusNum = Integer.parseInt(s);
+                break;
+            } catch (NumberFormatException e) {
+                try {
+                    throw new IllegalArgumentException();
+                } catch (IllegalArgumentException e2) {
+                    System.out.println("[ERROR] 잘못된 값을 입력했습니다. 다시 입력해주세요.");
+                }
+            }
+        }
+        return bonusNum;
+    }
 }
